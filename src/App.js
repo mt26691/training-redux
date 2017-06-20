fb1699891e448593654d82ee3ccb673b300275b8
Manh Tung <nguyenmanhtung848@gmail.com>
2017-06-20 16:58:31 +0700

    project set up

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { ConnectedRouter } from 'connected-react-router'
+// import { NormalRoutes, AdminRoutes } from './routes'
+import { Route, Switch } from 'react-router'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <ConnectedRouter history={this.props.history}>
+        <Switch>
+          <Route path="/" render={() =>
+            <div className="App">
+              <div className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <h2>Welcome to React</h2>
+              </div>
+              <p className="App-intro">
+                To get started, edit <code>src/App.js</code> and save to reload.
+            </p>
+            </div>
+          } />
+        </Switch>
+      </ConnectedRouter>
+
     );
   }
 }
+
+App.propTypes = {
+  history: PropTypes.object,
+}
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,28 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
+import { routerMiddleware, connectRouter } from 'connected-react-router'
+import { Provider } from 'react-redux'
+import rootReducer from './reducers'
+import reduxThunk from 'redux-thunk';
+import reduxPromise from 'redux-promise';
+import { applyMiddleware, compose, createStore } from 'redux'
+import history from './common/history';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const store = createStore(
+	connectRouter(history)(rootReducer),
+	compose(
+		applyMiddleware(
+			reduxThunk,
+			reduxPromise,
+			routerMiddleware(history),
+		),
+	),
+)
+
+ReactDOM.render(<Provider store={store}>
+		<App history={history} />
+	</Provider>
+	, document.getElementById('root'));
 registerServiceWorker();
